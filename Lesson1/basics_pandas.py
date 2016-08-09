9a87f79004157c11133147f680d4be5310a8e81a
Felix Perez <perezmarquezfelix@gmail.com>
2016-08-08 21:04:13 -0400

    Lesson 1 basics pandas data imputation

--- Lesson1/basics_pandas.py
+++ Lesson1/basics_pandas.py
@@ -282,3 +282,25 @@ def numpy_dot(df):
     return olympic_points_df
 
 print(numpy_dot(olympic_medal_counts))
+
+
+def imputation(filename):
+    """
+    Pandas dataframes have a method called 'fillna(value)', such that you can
+    pass in a single value to replace any NAs in a dataframe or series. You
+    can call it like this:
+        dataframe['column'] = dataframe['column'].fillna(value)
+
+    Using the numpy.mean function, which calculates the mean of a numpy
+    array, impute any missing values in our Lahman baseball
+    data sets 'weight' column by setting them equal to the average weight.
+
+    You can access the 'weight' colum in the baseball data frame by
+    calling baseball['weight']
+    """
+
+    baseball = pd.read_csv(filename)
+    value = np.mean(baseball['weight'])
+
+    baseball['weight'] = baseball['weight'].fillna(value)
+    return baseball
